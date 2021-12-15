273def42ad8e5df47021aaf0e1f4bf080d7862c8
Jeremy Leland <jeremy.leland@gmail.com>
2021-12-15 14:24:16 -0800

    Add player placeholder, match level and camera to original

--- src/ThreeGame.js
+++ src/ThreeGame.js
@@ -2,7 +2,7 @@ import * as THREE from '../lib/three.module.js';
 
 export class ThreeGame {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );  
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 500 );  
   
   constructor() {
     const renderer = new THREE.WebGLRenderer();
